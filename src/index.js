62f4da3c973a18da8ea8b1c509a0504a30cc87f8
Tae.Garam <drasla@naver.com>
2021-02-20 17:33:22 +0900

    # 1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state === 0) => {
+const countModifier = (count === 0, action) => {
     // Modify State
-    // ...
-    // Return State
-    return state;
+    if (action.type === "ADD") {
+        return count + 1;
+    } else if (action.type === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
 }
 
+// Store 생성
 const countStore = createStore(countModifier);
+
+// action 전달
+countStore.dispatch({type: "ADD"});
+
+// State 출력
 console.log(countStore.getState());
