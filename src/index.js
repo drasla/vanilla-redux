09c915aaff2ec3bd2dcc974ed09dc314758f3f99
Tae.Garam <drasla@naver.com>
2021-02-20 17:21:21 +0900

    # 1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
+import {createStore} from "redux";
+
 const plus = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-    number.innerText = count;
+const countModifier = (state === 0) => {
+    // Modify State
+    // ...
+    // Return State
+    return state;
 }
 
-const handleAdd = () => {
-    count = count + 1;
-    updateText();
-};
-
-const handleMinus = () => {
-    count = count - 1;
-    updateText();
-}
-
-plus.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifier);
+console.log(countStore.getState());
